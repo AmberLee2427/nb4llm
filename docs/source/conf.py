18db7822345feac9c0d3c762442062086a7c6b51
Amber <ambermalpas@gmail.com>
2025-07-22 23:33:42 -0400

    basic rtd setup

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "nb4llm"
+copyright = "2025, Amber Malpas (AmberLee2427)"
+author = "Amber Malpas (AmberLee2427)"
+release = "v0.1.3"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "python3.9+"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. nb4llm documentation master file, created by
+   sphinx-quickstart on Tue Jul 22 23:30:04 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to nb4llm's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
